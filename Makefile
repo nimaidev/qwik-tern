6568ac72000f4d7b84e08cf18a29cbd12725e3be
Nimai Charan <nimai.c@iqnext.io>
2024-10-22 16:21:31 +0530

    removed unecessary dir

--- Makefile
+++ Makefile
@@ -1,2 +1,13 @@
 run:
 	@python main.py
+
+clean:
+	@rm -R dist
+
+build:
+	@py -m build
+
+release:
+	twine upload dist/* --verbose
+
+
--- qwik_tern/constants/constants.py
+++
@@ -1,7 +0,0 @@
-
-
-CHECK_IF_TABLE_EXISTS = """ SELECT COUNT(*) 
-    FROM information_schema.tables 
-    WHERE table_schema = 'ai-service' 
-    AND table_name = 'db_changelog' 
-    """
