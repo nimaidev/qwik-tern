8c3f6a0b68a4da3eb9439adf42bee12cd3d75930
Nimai Charan <nimai.c@iqnext.io>
2024-10-22 18:48:47 +0530

    version 0.0.5.1

--- qwik_tern/__init__.py
+++ qwik_tern/__init__.py
@@ -3,18 +3,6 @@
 from qwik_tern.connection import get_connection_pool, create_internal_db
 from qwik_tern.migration import run_default_migration, run_migration_down
 
-# __all__ = [
-#     get_connection_pool,
-#     create_internal_db,
-#     process_migration,
-#     process_migration_down,
-#     insert_data,
-#     calculate_checksum,
-#     compare_checksum,
-#     read_migration_from_file
-# ]
-
-
 
 __all__ = [
     "get_connection_pool",
